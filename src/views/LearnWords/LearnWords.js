d9ceb26119e7bdb66cd5a5b2d1b347fb7470ba0c
saraboro <54246527+saraboro@users.noreply.github.com>
2021-03-03 16:55:32 +0200

    Update LearnWords.js
    Fix play button that is not working
    Clean up the code.

--- src/views/LearnWords/LearnWords.js
+++ src/views/LearnWords/LearnWords.js
@@ -1,33 +1,24 @@
 import React from "react";
 import firebaseService from 'firebase_services/firebaseService';
 import {Component} from 'react';
-// import { Card } from '@material-ui/core';
 import CardActions from '@material-ui/core/CardActions';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import { IconButton } from '@material-ui/core';
 import SettingsVoiceIcon from '@material-ui/icons/SettingsVoice';
 import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import CardHeader from '../../components/Card/CardHeader';
-import CardFooter from '../../components/Card/CardFooter';
 import RegularButton from '../../components/CustomButtons/Button'
 import Card from "../../components/Card/Card";
 import CardIcon from "../../components/Card/CardIcon";
-import GridItem from "components/Grid/GridItem";
-import GridContainer from "components/Grid/GridContainer";
 
 //get map size
 function getMapSize(x) {
     var len = 0;
-    for (var count in x) {
-            len++;
-    }
-    console.log("len ,", len, x);
-
+    for (var count in x)
+        len++;
     return len;
 }
 
@@ -38,14 +29,13 @@ function sortMapByKey(o) {
 
 //this componnent is a layout for the Learn Words page
 class LearnWords extends Component {
-
     constructor() {
         super();
         this.state = {
             words: {},
             grammerWords: {},
-            count: 0,
-            realHoldoingWord: 0,
+            relativeHoldoingWord: 0, //the relative word we are holding at
+            realHoldoingWord: 0, //the most advanced holding word that was learned
             wordsLength: 0,
             previousDisabled: false,
             nextDisabled: false
@@ -53,7 +43,6 @@ class LearnWords extends Component {
         this.handleNext = this.handleNext.bind(this);
         this.handlePrevious = this.handlePrevious.bind(this);
         this.play = this.play.bind(this);
-
     }
    
     componentDidMount() {
@@ -71,31 +60,29 @@ class LearnWords extends Component {
                 holdingWord = count; 
             })
             .then(() => {
-                
+                //at the first time the user starts to learn words, we need to update that we are at the #1 word
                 if(holdingWord === 0){
                     holdingWord++;
                     firebaseService.setHoldingWordsByCategoryForUser(this.props.location.state, holdingWord)
                 }
 
                 var wordsListLength = getMapSize(list.words);
-                // to show the word that the user is holding on
-                this.setState({count: holdingWord});
+                //to show the word that the user is holding on
+                this.setState({relativeHoldoingWord: holdingWord});
+                //to keep track of the most advanced word that the user learned
                 this.setState({realHoldoingWord: holdingWord});
 
                 //disable previous or next buttons if needed
                 //first word - could not press previous now
-                if(holdingWord === 0){
+                if(holdingWord === 1){
                     this.setState({previousDisabled: true});
                 }
                 //last word - could not press next anymore
-                if(holdingWord === wordsListLength - 1){
+                if(holdingWord === wordsListLength){
                     this.setState({nextDisabled: true});
                 }
-
                 //only now we will set the size, this will also indecate to show to screen
                 this.setState({wordsLength: wordsListLength});
-
-
             })
         })
     }
@@ -103,55 +90,53 @@ class LearnWords extends Component {
     //go back to previous word
     handlePrevious(event) {
         event.preventDefault();
-        if (this.state.count > 0){
+        if (this.state.relativeHoldoingWord > 0){
             //one before last word - could press next now
-            if (this.state.count === this.state.wordsLength - 1){
+            if (this.state.relativeHoldoingWord === this.state.wordsLength){
                 this.setState({nextDisabled: false});
             }
             //#1 word - could not press previous anymore
-            if (this.state.count === 1){
+            if (this.state.relativeHoldoingWord === 2){
                 this.setState({previousDisabled: true});
             }
-            var previous  = this.state.count -1;
-            //firebaseService.setHoldingWordsByCategoryForUser(this.props.location.state, previous)
-            this.setState({count: previous});
+            var previous  = this.state.relativeHoldoingWord -1;
+            this.setState({relativeHoldoingWord: previous});
         }
     }
 
     //continue to next word
     handleNext(event) {
         event.preventDefault();
-        if (this.state.count < this.state.wordsLength - 1){
+        if (this.state.relativeHoldoingWord < this.state.wordsLength){
             //#2 word - could press previous now
-            if (this.state.count === 0){
+            if (this.state.relativeHoldoingWord === 1){
                 this.setState({previousDisabled: false});
             }
             //last word - could not press next anymore
-            if (this.state.count === this.state.wordsLength - 2){
+            if (this.state.relativeHoldoingWord === this.state.wordsLength - 1){
                 this.setState({nextDisabled: true});
             }
-            var next  = this.state.count + 1;
-            if(this.state.realHoldoingWord == this.state.count){
+            var next  = this.state.relativeHoldoingWord + 1;
+            if(this.state.realHoldoingWord == this.state.relativeHoldoingWord){
                 firebaseService.setHoldingWordsByCategoryForUser(this.props.location.state, next)
                 this.setState({realHoldoingWord: next});
             }
-            this.setState({count: next});
+            this.setState({relativeHoldoingWord: next});
         }
     }
 
     //play the word with speech
     play(event) {
         event.preventDefault();
-        var u = new SpeechSynthesisUtterance(Object.keys(this.state.words)[this.state.count])
+        var u = new SpeechSynthesisUtterance(Object.keys(this.state.words)[this.state.relativeHoldoingWord-1])
         u.lang = 'en-GB';
+        speechSynthesis.cancel();
         speechSynthesis.speak(u);
     }
 
     render() {
         if (this.state.wordsLength === 0)
             return null;
-        
-        var grammerList = null;
 
         const styles = {
             'textAlign': "center",
@@ -159,42 +144,37 @@ class LearnWords extends Component {
             maxWidth: 500,
         };
 
-        //for category 3 we add the grammer words
+        //for category 3 we add the grammer words to the card
+        var grammerList = null;
         if(this.props.location.state === 'category3'){ 
-            grammerList = Object.keys(this.state.grammerWords[Object.keys(this.state.words)[this.state.count-1]]).map((key) => ( 
+            grammerList = Object.keys(this.state.grammerWords[Object.keys(this.state.words)[this.state.relativeHoldoingWord-1]]).map((key) => ( 
                 <ListItem>
                     <ListItemText
-                    primary={this.state.grammerWords[Object.keys(this.state.words)[this.state.count-1]][key] + " = " + key}
+                    primary={this.state.grammerWords[Object.keys(this.state.words)[this.state.relativeHoldoingWord-1]][key] + " = " + key}
                     />
                     <CardIcon>
                         <PriorityHighIcon />
                     </CardIcon>
                 </ListItem>
             ))
-            
         }
         return (
-           
-            <GridContainer>
-
-            <GridItem xs={12} sm={12} md={8} direction='row'>        
                 <Card style={styles}>
                     <div className="content" style={styles}>
                         <CardActions style={styles}>
                             <CardHeader color='info'>
-                                <SettingsVoiceIcon size="large" onClick={this.play}></SettingsVoiceIcon>
+                                <SettingsVoiceIcon size="large" onClick={this.play} disabled={false}></SettingsVoiceIcon>
                             </CardHeader>
                         </CardActions>
                         {  
                             <h3>    
-                                {"מילה: " + Object.keys(this.state.words)[this.state.count-1]}       
+                                {"מילה: " + Object.keys(this.state.words)[this.state.relativeHoldoingWord-1]}       
                                 <br />
-                                {"תרגום: " + this.state.words[Object.keys(this.state.words)[this.state.count-1]]}
+                                {"תרגום: " + this.state.words[Object.keys(this.state.words)[this.state.relativeHoldoingWord-1]]}
                                 <br />
                                 <List dense={true}>{grammerList}</List>
                             </h3>
                         }
-                       
                         <CardActions>
                             <RegularButton id="previous" color='info' aria-label="add an arrow" onClick={this.handlePrevious} disabled={this.state.previousDisabled}>
                                 <ArrowForwardIosIcon size="large"/>
@@ -205,8 +185,6 @@ class LearnWords extends Component {
                         </CardActions>
                     </div>
                 </Card>
-                </GridItem>
-            </GridContainer>
           );
       }
   }
